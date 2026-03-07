9e4bb1f4fe050c55f4d63885902d178c94769a1d
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-03-07 09:02:08 +0600

    created the lesson and other section

--- script/index.js
+++ script/index.js
@@ -4,6 +4,27 @@ const loadLessons = () => {
     .then((json) =>  displayLessons(json.data))
 }
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then((res) => res.json() )
+    .then((data) =>  displayLevelWord(data.data))
+}
+ const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+    words.forEach( (word) => {
+        
+        const card = document.createElement('div');
+        card.innerHTML = `
+        <p>cat</P>
+        `;
+
+        wordContainer.append(card);
+    } )
+ }
+
 const displayLessons = (lessons) => {
     // 1. get the container & empty
     const levelContainer = document.getElementById('level-Container');
@@ -14,7 +35,7 @@ const displayLessons = (lessons) => {
         //           3.create Element 
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-           <button class="btn btn-sm btn-outline btn-primary"
+           <button onclick="loadLevelWord(${lesson.level_no})"  class="btn btn-sm btn-outline btn-primary"
                 ><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
               </button>
         
